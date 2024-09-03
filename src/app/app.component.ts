3f007c6d82e4cd2d8674f47da0d6d1454d33fd87
Mihir <mihir@velt.dev>
2024-09-03 09:43:35 +0530

    Added dummy wireframe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DocumentComponent } from './component/document/document.component';
+import { VeltService } from './services/velt.service';
+import { AuthService } from './services/auth.service';
+import { ToolbarComponent } from "./component/toolbar/toolbar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet, DocumentComponent, ToolbarComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
+	schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppComponent {
-  title = 'slides';
+export class AppComponent implements OnInit {
+	title = 'slides';
+
+	constructor(
+		private veltService: VeltService,
+		private authService: AuthService,
+	) { }
+
+	async ngOnInit(): Promise<void> {
+		// Follow the Setup Guide for more info: https://docs.velt.dev/get-started/setup/install
+
+		await this.veltService.initializeVelt('AN5s6iaYIuLLXul0X4zf');
+
+		const user = this.authService.getUser()(); // Getting Random User
+		if (user) {
+			await this.veltService.identifyUser(user);
+		}
+
+		await this.veltService.setDocument('slides', { documentName: 'slides' });
+		this.veltService.setDarkMode(true);
+	}
 }
